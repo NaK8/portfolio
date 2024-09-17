e6ac8e1a16d60377fd99c9dfa05e9b4040d6aa4d
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-09-18 02:11:15 +0500

    add testimonial section animation

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -6,36 +6,37 @@ import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import SectionHeader from "@/components/section-header";
 import Image from "next/image";
 import Card from "@/components/card";
+import { Fragment } from "react";
 
 const testimonials = [
   {
     name: "Alex Turner",
     position: "Marketing Manager @ TechStartups",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    text: "Nabeel was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
     avatar: memojiAvatar1,
   },
   {
     name: "Olivia Green",
     position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    text: "Working with Nabeel was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
     avatar: memojiAvatar2,
   },
   {
     name: "Daniel White",
     position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    text: "Nabeel's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
     avatar: memojiAvatar3,
   },
   {
     name: "Emily Carter",
     position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    text: "Nabeel is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
     avatar: memojiAvatar4,
   },
   {
     name: "Michael Brown",
     position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    text: "Nabeel's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
   },
 ];
@@ -50,30 +51,34 @@ const TestimonialsSection = () => {
           description="Don't just take my words for it. See what my clients have to say about
           my work"
         />
-        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-24">
-          <div className="flex flex-none gap-8">
-            {testimonials.map((testimonial) => (
-              <Card
-                key={crypto.randomUUID()}
-                className="max-w-xs p-6 md:max-w-md md:p-8"
-              >
-                <div className="flex items-center gap-4">
-                  <div className="inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-gray-700">
-                    <Image
-                      src={testimonial.avatar}
-                      alt={testimonial.name}
-                      className="max-h-full"
-                    />
-                  </div>
-                  <div>
-                    <div className="font-semibold">{testimonial.name}</div>
-                    <div className="text-sm text-white/40 md:text-base">
-                      {testimonial.position}
+        <div className="-my-4 mt-12 flex overflow-x-clip py-4 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-20">
+          <div className="animate-move-left flex flex-none gap-8 pr-8 [animation-duration:60s] hover:[animation-play-state:paused]">
+            {[...new Array(2)].fill(0).map((_, i) => (
+              <Fragment key={i}>
+                {testimonials.map((testimonial) => (
+                  <Card
+                    key={crypto.randomUUID()}
+                    className="max-w-xs p-6 transition duration-300 hover:-rotate-3 md:max-w-md md:p-8"
+                  >
+                    <div className="flex items-center gap-4">
+                      <div className="inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-gray-700">
+                        <Image
+                          src={testimonial.avatar}
+                          alt={testimonial.name}
+                          className="max-h-full"
+                        />
+                      </div>
+                      <div>
+                        <div className="font-semibold">{testimonial.name}</div>
+                        <div className="text-sm text-white/40 md:text-base">
+                          {testimonial.position}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <p className="mt-4 text-sm md:mt-6">{testimonial.text}</p>
-              </Card>
+                    <p className="mt-4 text-sm md:mt-6">{testimonial.text}</p>
+                  </Card>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
