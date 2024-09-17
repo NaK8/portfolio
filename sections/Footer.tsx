67b4e631c1c48183ab02e321a4b95eeeee2849e6
Nabeel Ahmed <nabeelahmedkhan999@gmail.com>
2024-09-18 04:28:23 +0500

    fix footer bug

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -21,10 +21,10 @@ const footerLinks = [
 
 const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
-      <div className="pointer-events-none absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
+    <footer className="relative z-0 overflow-x-clip">
+      <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
       <div className="container">
-        <div className="z-10 flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
+        <div className="flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
           <div className="text-white/40">&copy; 2024 All rights reserved</div>
           <nav className="flex flex-col items-center gap-8 md:flex-row">
             {footerLinks.map((link) => (
@@ -32,6 +32,7 @@ const Footer = () => {
                 href={link.href}
                 key={crypto.randomUUID()}
                 className="inline-flex items-center gap-1.5"
+                target="_blank"
               >
                 <span className="font-semibold">{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
